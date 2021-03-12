8b5a05dd523b6b093d43512cb53855fefc306b4d
manosmata <61359789+manosmata@users.noreply.github.com>
2021-03-12 17:28:24 +0200

    Add files via upload

--- files/webdb.sql
+++ files/webdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 05, 2021 at 02:56 PM
+-- Generation Time: Mar 12, 2021 at 04:27 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.2.34
 
@@ -20,90 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `webdb`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `entries`
---
-
-CREATE TABLE `entries` (
-  `identries` int(50) NOT NULL,
-  `timings` time NOT NULL,
-  `entriescol` varchar(200) NOT NULL,
-  `serverIPaddress` varchar(200) NOT NULL,
-  `startedDateTime` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `fileinfo`
---
-
-CREATE TABLE `fileinfo` (
-  `idfileinfo` int(15) NOT NULL,
-  `provider` varchar(200) NOT NULL,
-  `ipaddress` varchar(200) NOT NULL,
-  `dateoffile` date NOT NULL,
-  `idresponse` int(50) NOT NULL,
-  `idrequest` int(50) NOT NULL,
-  `identries` int(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `headers`
---
-
-CREATE TABLE `headers` (
-  `idheaders` int(50) NOT NULL,
-  `contentType` varchar(200) NOT NULL,
-  `cacheControl` varchar(200) NOT NULL,
-  `pragma` varchar(200) NOT NULL,
-  `expires` varchar(200) NOT NULL,
-  `age` int(11) NOT NULL,
-  `lastModified` date NOT NULL,
-  `host` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `request`
---
-
-CREATE TABLE `request` (
-  `idrequest` int(50) NOT NULL,
-  `url` varchar(200) NOT NULL,
-  `method` varchar(200) NOT NULL,
-  `idheaders` int(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `response`
---
-
-CREATE TABLE `response` (
-  `idresponse` int(50) NOT NULL,
-  `statusText` varchar(500) NOT NULL,
-  `idheaders2` int(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `timings`
---
-
-CREATE TABLE `timings` (
-  `idtimings` int(20) NOT NULL,
-  `wait` time NOT NULL,
-  `identries` int(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -125,61 +41,16 @@ CREATE TABLE `userinfo` (
 --
 
 INSERT INTO `userinfo` (`iduserinfo`, `username`, `password`, `email`, `city`, `idfileinfo`) VALUES
-(1, 'xrysa', 'XRYSA123@', 'xrysa@tsouki.com', '', 0),
-(2, 'manos', 'MANOS123@', 'manos@matakias.com', '', 0),
-(3, 'nikol', 'NIKOL123@', 'nikol@sklav.com', '', 0),
-(4, 'rico', 'RICO123@', 'rico@thedog.com', '', 0),
-(5, 'java', 'JAVA123@', 'java@theotherdog.com', '', 0),
-(6, 'bigbrother', 'BIG123@@', 'big@brother.com', '', 0);
+(1, 'bigbrother', 'BIG123@@', 'big@brother.com', '', 0),
+(2, 'java', 'JAVA123@', 'java@theotherdog.com', '', 0),
+(3, 'rico', 'RICO123@', 'rico@thedog.com', '', 0),
+(4, 'nikol', 'NIKOL123@', 'nikol@sklav.com', '', 0),
+(5, 'manos', 'MANOS123@', 'manos@matakias.com', '', 0),
+(6, 'xrysa', 'XRYSA123@', 'xrysa@tsouki.com', '', 0);
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `entries`
---
-ALTER TABLE `entries`
-  ADD PRIMARY KEY (`identries`),
-  ADD KEY `identries` (`identries`);
-
---
--- Indexes for table `fileinfo`
---
-ALTER TABLE `fileinfo`
-  ADD PRIMARY KEY (`idfileinfo`),
-  ADD KEY `idresponse` (`idresponse`,`idrequest`,`identries`),
-  ADD KEY `idrequest` (`idrequest`),
-  ADD KEY `identries` (`identries`);
-
---
--- Indexes for table `headers`
---
-ALTER TABLE `headers`
-  ADD PRIMARY KEY (`idheaders`),
-  ADD KEY `idheaders` (`idheaders`);
-
---
--- Indexes for table `request`
---
-ALTER TABLE `request`
-  ADD PRIMARY KEY (`idrequest`),
-  ADD KEY `idheaders` (`idheaders`);
-
---
--- Indexes for table `response`
---
-ALTER TABLE `response`
-  ADD PRIMARY KEY (`idresponse`),
-  ADD KEY `idheaders` (`idheaders2`);
-
---
--- Indexes for table `timings`
---
-ALTER TABLE `timings`
-  ADD PRIMARY KEY (`idtimings`),
-  ADD KEY `identries` (`identries`),
-  ADD KEY `identries_2` (`identries`);
 
 --
 -- Indexes for table `userinfo`
@@ -193,62 +64,10 @@ ALTER TABLE `userinfo`
 --
 
 --
--- AUTO_INCREMENT for table `response`
---
-ALTER TABLE `response`
-  MODIFY `idresponse` int(50) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `timings`
---
-ALTER TABLE `timings`
-  MODIFY `idtimings` int(20) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `userinfo`
 --
 ALTER TABLE `userinfo`
-  MODIFY `iduserinfo` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `entries`
---
-ALTER TABLE `entries`
-  ADD CONSTRAINT `entries_ibfk_1` FOREIGN KEY (`identries`) REFERENCES `fileinfo` (`identries`);
-
---
--- Constraints for table `fileinfo`
---
-ALTER TABLE `fileinfo`
-  ADD CONSTRAINT `fileinfo_ibfk_1` FOREIGN KEY (`idfileinfo`) REFERENCES `userinfo` (`idfileinfo`);
-
---
--- Constraints for table `headers`
---
-ALTER TABLE `headers`
-  ADD CONSTRAINT `headers_ibfk_1` FOREIGN KEY (`idheaders`) REFERENCES `request` (`idheaders`);
-
---
--- Constraints for table `request`
---
-ALTER TABLE `request`
-  ADD CONSTRAINT `request_ibfk_1` FOREIGN KEY (`idrequest`) REFERENCES `fileinfo` (`idrequest`);
-
---
--- Constraints for table `response`
---
-ALTER TABLE `response`
-  ADD CONSTRAINT `response_ibfk_2` FOREIGN KEY (`idheaders2`) REFERENCES `fileinfo` (`idresponse`);
-
---
--- Constraints for table `timings`
---
-ALTER TABLE `timings`
-  ADD CONSTRAINT `timings_ibfk_1` FOREIGN KEY (`identries`) REFERENCES `entries` (`identries`);
+  MODIFY `iduserinfo` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=76;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
