c62d270dc5cf9d993e096b3db58f6c2d71bac97f
TsChrys <31798837+TsChrys@users.noreply.github.com>
2021-03-23 17:41:00 +0200

    Add files via upload

--- files/webdb.sql
+++ files/webdb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.3
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 12, 2021 at 04:27 PM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.2.34
+-- Generation Time: Mar 23, 2021 at 04:40 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,22 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `harfiles`
+--
+
+CREATE TABLE `harfiles` (
+  `idharfiles` int(15) NOT NULL,
+  `hartext` text NOT NULL,
+  `city` varchar(300) NOT NULL,
+  `date` date NOT NULL,
+  `ipaddress` varchar(300) NOT NULL,
+  `provider` varchar(300) NOT NULL,
+  `iduserinfo` int(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `userinfo`
 --
 
@@ -31,43 +47,63 @@ CREATE TABLE `userinfo` (
   `iduserinfo` int(15) NOT NULL,
   `username` varchar(200) NOT NULL,
   `password` varchar(200) NOT NULL,
-  `email` varchar(200) NOT NULL,
-  `city` varchar(100) NOT NULL,
-  `idfileinfo` int(50) NOT NULL
+  `email` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `userinfo`
 --
 
-INSERT INTO `userinfo` (`iduserinfo`, `username`, `password`, `email`, `city`, `idfileinfo`) VALUES
-(1, 'bigbrother', 'BIG123@@', 'big@brother.com', '', 0),
-(2, 'java', 'JAVA123@', 'java@theotherdog.com', '', 0),
-(3, 'rico', 'RICO123@', 'rico@thedog.com', '', 0),
-(4, 'nikol', 'NIKOL123@', 'nikol@sklav.com', '', 0),
-(5, 'manos', 'MANOS123@', 'manos@matakias.com', '', 0),
-(6, 'xrysa', 'XRYSA123@', 'xrysa@tsouki.com', '', 0);
+INSERT INTO `userinfo` (`iduserinfo`, `username`, `password`, `email`) VALUES
+(1, 'bigbrother', 'BIG123@@', 'big@brother.com'),
+(2, 'java', 'JAVA123@', 'java@theotherdog.com'),
+(3, 'rico', 'RICO123@', 'rico@thedog.com'),
+(4, 'nikol', 'NIKOL123@', 'nikol@sklav.com'),
+(5, 'manos', 'MANOS123@', 'manos@matakias.com'),
+(6, 'xrysa', 'XRYSA123@', 'xrysa@tsouki.com');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `harfiles`
+--
+ALTER TABLE `harfiles`
+  ADD PRIMARY KEY (`idharfiles`),
+  ADD KEY `iduserinfo` (`iduserinfo`);
+
+--
 -- Indexes for table `userinfo`
 --
 ALTER TABLE `userinfo`
-  ADD PRIMARY KEY (`iduserinfo`),
-  ADD KEY `idfileinfo` (`idfileinfo`);
+  ADD PRIMARY KEY (`iduserinfo`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `harfiles`
+--
+ALTER TABLE `harfiles`
+  MODIFY `idharfiles` int(15) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `userinfo`
 --
 ALTER TABLE `userinfo`
-  MODIFY `iduserinfo` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=76;
+  MODIFY `iduserinfo` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `harfiles`
+--
+ALTER TABLE `harfiles`
+  ADD CONSTRAINT `harfiles_ibfk_1` FOREIGN KEY (`iduserinfo`) REFERENCES `userinfo` (`iduserinfo`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
